65aa41d11174c8e6436250b2fc51d8c5fe332fb8
drn-sharma99 <drn.sharma99@gmail.com>
2024-09-17 11:46:19 +0530

    day6_Q1 uploaded

---
+++ day6_Q_1_assign.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+class Date
+{
+    int day;
+    int month;
+    int year;
+
+public:
+Date()
+{
+    day=10;
+    month=12;
+    year=1996;
+}
+Date(int x,int y,int z)
+{
+    day=x;
+    month=y;
+    year=z;
+}
+void acceptDate()
+{
+    cout<<"Enter date DD-MM-YY :"<<endl;
+    cin>>day>>month>>year;
+}
+void DisplayDate()
+{
+   cout<<" Date :"<<day<<"-"<<month<<"-"<<year<<endl;
+}
+
+};
+class Person
+{
+   string name;
+   string address;
+   Date dob;
+public:
+Person()
+{
+    name="AAAA";
+    address="zxzxzxzxz";
+}
+Person(string n,string a)
+{
+    this->name=n;
+    this->address=a;
+    
+
+}
+void accept()
+{
+    cout << "Enter the name - ";
+    cin >> name;
+    cout << "Enter the address - ";
+    cin >> address;
+    dob.acceptDate();
+}
+void display()
+{
+    cout<<"*****************************************"<<endl;
+    cout<<"Person Detail :"<<endl<<"Name :"<<name<<endl <<"Address :"<<address<<endl;
+    cout<<"Date of Birth :";
+    dob.DisplayDate();
+}
+};
+class Employee :public Person
+{
+    int empid;
+    double salary;
+    string dept;
+    Date doj;
+public:
+Employee()
+{
+    cout<<"Employee()"<<endl;
+    empid=0;
+    salary=0;
+    dept="ee";
+}
+Employee(int empid,double salary,string dept,string name,string address):Person(name,address)
+{
+    this->empid=empid;
+    this->salary=salary;
+    this->dept=dept;
+
+}
+    void accept()
+    {
+        cout << "Enter empid - ";
+        cin >> empid;
+        Person::accept();
+        cout << "Enter empsalary - ";
+        cin >> salary;
+        cout << "Enter empdept - ";
+        cin >> dept;
+        
+    }
+    void display()
+    {
+        cout << "Empid - " << empid << endl;
+        Person::display();
+        cout << "Emp Salary - " << salary << endl;
+        cout << "Emp Dept " << dept << endl;
+    }
+};
+
+int main()
+{
+   Employee e1;
+   e1.accept();
+   e1.display();
+   return 0;
+}
